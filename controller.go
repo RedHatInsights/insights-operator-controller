92bf72ee5d9d26b23bdc692a040492f68cce03da
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-11 18:39:28 +0200

    Initialize the server properly

--- controller.go
+++ controller.go
@@ -5,9 +5,14 @@ import (
 	"github.com/redhatinsighs/insights-operator-controller/storage"
 )
 
+// Entry point to the Insights operator controller.
+// It performs several tasks:
+// - connect to the storage with basic test if storage is accessible
+// - start the HTTP server with all required endpints
+// - TODO: initialize connection to the logging service
 func main() {
 	storage := storage.New("sqlite3", "./controller.db")
 	defer storage.Close()
 
-	server.Initialize(":8080")
+	server.Initialize(":8080", storage)
 }
