9effe2c737de1dfbe6f080f5e8ed89106f0fabe5
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-11 18:34:30 +0200

    Storage methods to retrieve data from the database

--- storage/storage.go
+++ storage/storage.go
@@ -30,21 +30,103 @@ func (storage Storage) Close() {
 	}
 }
 
-func ListOfClusters() {
-	rows, err := connections.Query("SELECT id, cluster FROM cluster")
+type Cluster struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
 
-	var uid int
-	var cluster string
+type ConfigurationProfile struct {
+	Id            int    `json:"id"`
+	Configuration string `json:"configuration"`
+	ChangedAt     string `json:"changed_at"`
+	ChangedBy     string `json:"changed_by"`
+	Description   string `json:"description"`
+}
+
+type ClusterConfiguration struct {
+	Id            int    `json:"id"`
+	Cluster       string `json:"cluster"`
+	Configuration string `json:"configuration"`
+	ChangedAt     string `json:"changed_at"`
+	ChangedBy     string `json:"changed_by"`
+	Active        string `json:"active"`
+	Reason        string `json:"reason"`
+}
+
+func (storage Storage) ListOfClusters() []Cluster {
+	clusters := []Cluster{}
+
+	rows, err := storage.connections.Query("SELECT id, name FROM cluster")
+	defer rows.Close()
 
 	for rows.Next() {
-		err = rows.Scan(&uid, &cluster)
+		var id int
+		var name string
+
+		err = rows.Scan(&id, &name)
 		if err == nil {
-			fmt.Println(uid)
-			fmt.Println(cluster)
+			clusters = append(clusters, Cluster{id, name})
+		} else {
+			log.Println("error", err)
+		}
+	}
+	rows.Close()
+
+	return clusters
+}
+
+func (storage Storage) ListConfigurationProfiles() []ConfigurationProfile {
+	profiles := []ConfigurationProfile{}
+
+	rows, err := storage.connections.Query("SELECT id, configuration, changed_at, changed_by, description FROM configuration_profile")
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var configuration string
+		var changed_at string
+		var changed_by string
+		var description string
+
+		err = rows.Scan(&id, &configuration, &changed_at, &changed_by, &description)
+		if err == nil {
+			profiles = append(profiles, ConfigurationProfile{id, configuration, changed_at, changed_by, description})
 		} else {
 			log.Println("error", err)
 		}
 	}
 
+	return profiles
+}
+
+func (storage Storage) ListClusterConfiguration(cluster string) []ClusterConfiguration {
+	configurations := []ClusterConfiguration{}
+
+	rows, err := storage.connections.Query(`
+SELECT operator_configuration.id, cluster.name, configuration, changed_at, changed_by, active, reason
+  FROM operator_configuration, cluster
+    ON cluster.id = operator_configuration.cluster
+ WHERE cluster.name=?`, cluster)
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var cluster string
+		var configuration string
+		var changed_at string
+		var changed_by string
+		var active string
+		var reason string
+
+		err = rows.Scan(&id, &cluster, &configuration, &changed_at, &changed_by, &active, &reason)
+		if err == nil {
+			configurations = append(configurations, ClusterConfiguration{id, cluster, configuration, changed_at, changed_by, active, reason})
+		} else {
+			log.Println("error", err)
+		}
+	}
 	rows.Close()
+
+	return configurations
 }
