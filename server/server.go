020ea377416cf5e1601d587c65d36be3a1ac83d0
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-29 13:34:27 +0100

    Logging actions for cluster operations

--- server/server.go
+++ server/server.go
@@ -107,7 +107,7 @@ func Initialize(address string, storage storage.Storage, splunk logging.Client) 
 	// REST API endpoints used by insights operator
 	// (handlers are implemented in the file operator.go)
 	operatorRouter := router.PathPrefix(API_PREFIX + "operator").Subrouter()
-	operatorRouter.HandleFunc("/register/{cluster}", func(w http.ResponseWriter, r *http.Request) { registerCluster(w, r, storage) }).Methods("GET", "PUT")
+	operatorRouter.HandleFunc("/register/{cluster}", func(w http.ResponseWriter, r *http.Request) { registerCluster(w, r, storage, splunk) }).Methods("GET", "PUT")
 	operatorRouter.HandleFunc("/configuration/{cluster}", func(w http.ResponseWriter, r *http.Request) { readConfigurationForOperator(w, r, storage) }).Methods("GET")
 
 	// Prometheus metrics
