67a20df66795ddc9f3edb2bc02e9b5e00c62c70f
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-11-04 09:55:47 +0100

    Updated script to create real database

--- local_storage/create_database.sh
+++ local_storage/create_database.sh
@@ -5,3 +5,4 @@ DATABASE=controller.db
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 cat "${SCRIPT_DIR}/schema.sql" | sqlite3 "${SCRIPT_DIR}/../${DATABASE}"
+cat "${SCRIPT_DIR}/init_data.sql" | sqlite3 "${SCRIPT_DIR}/../${DATABASE}"
