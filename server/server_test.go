8667c8c9d85c77fe825393db6627d29ff5911ed4
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-11-12 11:15:25 +0100

    Basic server REST API tests

--- server/server_test.go
+++ server/server_test.go
@@ -15,23 +15,3 @@ limitations under the License.
 */
 
 package server
-
-import (
-	"net/http"
-	"testing"
-)
-
-func TestMainEndpoint(t *testing.T) {
-	if !RunServiceTests {
-		return
-	}
-	response, err := http.Get(API_URL)
-	if err != nil {
-		t.Errorf("Communication error with the server %v", err)
-		return
-	}
-	if response.StatusCode != http.StatusOK {
-		t.Errorf("Expected HTTP status 200 OK, got %d", response.StatusCode)
-		return
-	}
-}
