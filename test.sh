658a2cfc817797c90f9d981afe89f56dba77f88f
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-11-12 11:02:13 +0100

    Updated script that starts REST API tests

--- test.sh
+++ test.sh
@@ -2,29 +2,50 @@ go build
 
 if [ $? -eq 0 ]
 then
-    echo "Build ok"
-    echo "Creating test database"
-    rm -f test.db
-    ./local_storage/create_test_database.sh
-    echo "Done"
-    echo "Starting service"
-    ./insights-operator-controller --storage test.db &
-    PID=$(echo $!)
-    if [ $? -eq 0 ]
-    then
-        echo "Service started, PID=$PID"
-        export RUN_SERVICE_TESTS=y
-        go test ./... -count=1 github.com/redhatinsighs/insights-operator-controller/server
-        kill $PID
-        if [ $? -eq 0 ]
-        then
-            echo "Service killed, PID=$PID"
-        else
-            echo "Fatal, can not kill a service process"
-        fi
-    else
-        echo "Fatal, can not start service"
-    fi
+    echo "Service build ok"
 else
     echo "Build failed"
+    exit 1
 fi
+
+go build -o rest-api-tests tests/rest_api_tests.go
+
+if [ $? -eq 0 ]
+then
+    echo "REST API tests build ok"
+else
+    echo "Build failed"
+    exit 1
+fi
+
+echo "Creating test database"
+rm -f test.db
+./local_storage/create_test_database.sh
+echo "Done"
+
+echo "Starting service"
+./insights-operator-controller --storage test.db &
+PID=$(echo $!)
+
+if [ $? -eq 0 ]
+then
+    echo "Service started, PID=$PID"
+    sleep 2
+
+    echo -e "------------------------------------------------------------------------------------------------"
+    ./rest-api-tests
+    EXIT_VALUE=$?
+    echo -e "------------------------------------------------------------------------------------------------"
+
+    kill $PID
+    if [ $? -eq 0 ]
+    then
+        echo "Service killed, PID=$PID"
+    else
+        echo "Fatal, can not kill a service process"
+    fi
+else
+    echo "Fatal, can not start service"
+fi
+
+exit $EXIT_VALUE
