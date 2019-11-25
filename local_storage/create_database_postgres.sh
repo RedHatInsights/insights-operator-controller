854e41437dd36151d25698d73c5c2f8e20126976
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-11-25 15:43:20 +0100

    Scripts to create controller database

---
+++ local_storage/create_database_postgres.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+SUPERUSER=postgres
+SU_PASSWORD=postgres
+
+DATABASE=controller
+DB_SERVER=localhost
+
+USER=tester
+USER_PASSWORD=tester
+
+
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+
+# create user and database
+psql "postgresql://${SUPERUSER}:${SU_PASSWORD}@${DB_SERVER}" -c "CREATE DATABASE ${DATABASE};" 
+psql "postgresql://${SUPERUSER}:${SU_PASSWORD}@${DB_SERVER}" -c "CREATE USER ${USER} PASSWORD '${USER_PASSWORD}';"  
+
+#create schema 
+cat "${SCRIPT_DIR}/schema_postgres.sql" | psql  "postgresql://${SUPERUSER}:${SU_PASSWORD}@${DB_SERVER}/${DATABASE}" 
+
+# grant priviliges to user 
+psql "postgresql://${SUPERUSER}:${SU_PASSWORD}@${DB_SERVER}/${DATABASE}" -c "GRANT  SELECT, INSERT, UPDATE,  DELETE 
+    ON  ALL TABLES IN SCHEMA public
+    TO  ${USER}; 
+	GRANT  SELECT,  UPDATE
+    ON  ALL SEQUENCES IN SCHEMA public 
+    TO  ${USER}; 
+    GRANT USAGE ON SCHEMA public TO ${USER};"
+
+# insert data as user 
+cat "${SCRIPT_DIR}/test_data_postgres.sql" | psql "postgresql://${USER}:${USER_PASSWORD}@${DB_SERVER}/${DATABASE}" 
+
