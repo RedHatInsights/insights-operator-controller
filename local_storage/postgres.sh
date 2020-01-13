c30acc09454c1acb25a9a07693250510098d784e
Martin Kunc <mkunc@dhcp129-129.brq.redhat.com>
2020-01-13 09:04:05 +0100

    Revert "add dockerfile folder path"
    This reverts commit c9d68f6f9a46e8d95d88db4ee9693344ae1190cc.

--- local_storage/postgres.sh
+++ local_storage/postgres.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-DIR=$(dirname "$0")
-docker build -f $DIR/Dockerfile.postgres -t rhc-postgres -t rhc-postgres .
+docker build -f .local/Dockerfile.postgres -t rhc-postgres -t rhc-postgres .
 
 docker run --name rhc-postgres -p 5432:5432 --rm -d rhc-postgres
