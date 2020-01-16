b8fbb6dd5d4aba913e3bdc25a3f4ba0a1333fcb4
dpensier <dpensier@localhost.localdomain>
2020-01-16 16:40:44 +0100

    test-benchmark.sh writes to a json file

--- test-benchmark.sh
+++ test-benchmark.sh
@@ -2,13 +2,14 @@
 
 export DBDRIVER=$1
 
-echo starting benchmark testing
 echo $DBDRIVER
 if [[ $DBDRIVER != 'sqlite3' ]] && [[ $DBDRIVER != 'postgres' ]]
 then
-	echo 'usage test-benchmark.sh [sqlite3|postgres]'
+	echo 'usage test-benchmark.sh sqlite3|postgres'
 	exit 1
 fi
+
+echo starting benchmark testing
 
 if [[ $DBDRIVER == 'sqlite3' ]]
 then
@@ -18,8 +19,9 @@ then
 
 	rm ./test.db
 	./local_storage/create_test_database_sqlite.sh
-	echo 'starting tests'
-	go test -bench=. ./tests/benchmark
+	echo 'performing tests, please wait'
+	go test -json -bench=. ./tests/benchmark > benchmark_test_out.json
+	echo 'benchmark tests completed!'
 fi
 
 if [[ $DBDRIVER == 'postgres' ]]
@@ -30,6 +32,7 @@ then
 	./local_storage/drop_test_database_postgres.sh
 	./local_storage/create_test_database_postgres.sh
 
-	echo 'starting tests'
-	go test -bench=. ./tests/benchmark
+	cho 'performing tests, please wait'
+	go test -json -bench=. ./tests/benchmark > benchmark_test_out.json
+	echo 'benchmark tests completed!'
 fi
