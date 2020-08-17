c4259edbe501e5a3fd7153d419b1cf795b4bfc01
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-17 11:58:57 +0200

    Fixed issue #379

--- server/trigger.go
+++ server/trigger.go
@@ -16,6 +16,12 @@ limitations under the License.
 
 package server
 
+// Generated documentation is available at:
+// https://godoc.org/github.com/RedHatInsights/insights-operator-controller/server
+//
+// Documentation in literate-programming-style is available at:
+// https://redhatinsights.github.io/insights-operator-controller/packages/server/trigger.html
+
 import (
 	"fmt"
 	"net/http"
