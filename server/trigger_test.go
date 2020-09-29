5b5aed4725edbd1920768c470b8d68f7a0c3f029
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-29 09:34:38 +0200

    Link to generated documentation

--- server/trigger_test.go
+++ server/trigger_test.go
@@ -16,6 +16,9 @@ limitations under the License.
 
 package server_test
 
+// Documentation in literate-programming-style is available at:
+// https://redhatinsights.github.io/insights-operator-controller/packages/server/trigger_test.html
+
 import (
 	"net/http"
 	"testing"
